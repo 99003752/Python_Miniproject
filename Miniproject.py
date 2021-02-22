01c84a5b53573ecc2bb317af210efa95f0acef01
99003752 <78851798+99003752@users.noreply.github.com>
2021-02-22 12:59:55 +0530

    verifing the output by different keywords

--- Miniproject.py
+++ Miniproject.py
@@ -1,15 +1,19 @@
+'''
+Discription:This file contains the code to count the Keywords in file'''
 import re
 Input_file=open("Input.txt")
 Keyword=input("Enter the word to search\n")
 count=0
-for line in Input_file:
-    file_line=Input_file.readline()
-    match=re.findall(Keyword,file_line,re.M|re.I)
-    if match:
-        count+=1
-        print(line)
-        print(file_line)
-    else:
-        continue
-print(count)
+read_file=Input_file.read()
+match=re.findall(Keyword,read_file,re.M|re.I)
+if match:
+    print(len(match))
+#file_line=Input_file.readlines()
+#for line in file_line:
+#    match=re.findall(Keyword,line,re.M|re.I)
+#    if match:
+ #       count+=1
+       # print(line)
+       # ripnt(file_line)
+#print(count)
 Input_file.close()
