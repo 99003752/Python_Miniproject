ed3c74ae2974950b6feaf79f7d7c8db48c5720a3
99003752 <78851798+99003752@users.noreply.github.com>
2021-02-23 09:42:51 +0530

    Some changes for printing line

--- Miniproject.py
+++ Miniproject.py
@@ -11,14 +11,17 @@ read_file=Input_file.read()
 match=re.findall(Keyword,read_file,re.M|re.I)
 if match:
     print(len(match))
-#file_line=Input_file.readlines()
-#for line in file_line:
-#    match=re.findall(Keyword,line,re.M|re.I)
-#    if match:
- #       count+=1
-       # print(line)
-       # print(file_line)
-#print(count)
+'''Input_file.close()
+Input_file=open("Input.txt")
+file_line=Input_file.readlines()
+for line in file_line:
+    match=re.findall(Keyword,line,re.M|re.I)
+    if match:
+        count+=1
+        print(line)
+        print(file_line)
+print(count)
+Input_file.close()'''
 Output_file_name=Keyword+".txt"
 Output_file=open(Output_file_name,"w")
 Output_file.write(str(len(match)))
