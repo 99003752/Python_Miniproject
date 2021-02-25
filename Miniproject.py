4bce0fe2ae50e58aa4e3d5b30edca38a1ac7e1fe
99003752 <78851798+99003752@users.noreply.github.com>
2021-02-25 17:22:45 +0530

    Taking n keywords

--- Miniproject.py
+++ Miniproject.py
@@ -4,31 +4,38 @@ Author:Sushma
 Date:22/02/2021
 """
 import re
-class myclass:
-    def __init__(self):
-        self.Keyword=input("Enter keyword\n")
-        self.Output_file_name = self.Keyword+".txt"
+import os
+class myclass: 
+    def __init__(self,Keyword):
+        self.Keyword=Keyword
         self.Input_file=open("Input.txt","r")
-        self.Output_file = open(self.Output_file_name, "w")
         self.count=0
+        self.Output_file_name = self.Keyword+".txt"
+        self.Output_file = open(self.Output_file_name, "w")
     def File_read(self):
         file_read=self.Input_file.read()
         file=file_read.split()
-        file1=re.split(r"\W",str(file))
+        file1=re.split(r"\W+",str(file))
         return file1
     def Match_Keyword(self,file1):
         for i in range(len(file1)):
             match=re.fullmatch(self.Keyword,file1[i],re.M|re.I)
             if match:
                 self.count+=1 
-                str1=file1[i-8]+" "+file1[i-4]+" "+file1[i]+" "+file1[i+4]+" "+file1[i+8]+" "
+                str1=file1[i-1]+" "+file1[i]+" "+file1[i+1]+" "
                 self.Output_file.write(str(str1))
                 self.Output_file.write("\n")
+        self.Output_file.write("Total number of "+self.Keyword+" Found are ")
         self.Output_file.write(str(self.count))
     def Close_file(self):
         self.Input_file.close()
         self.Output_file.close()
-myobj=myclass()
-file1=myobj.File_read()
-myobj.Match_Keyword(file1)
-myobj.Close_file()
+if __name__=="__main__":
+    number=int(input("How many keywords you want to enter\n"))
+    while number!=0:
+        Keyword=input("Enter Keyword\n")
+        myobj=myclass(Keyword)
+        file1=myobj.File_read()
+        myobj.Match_Keyword(file1)
+        number-=1
+    myobj.Close_file()
